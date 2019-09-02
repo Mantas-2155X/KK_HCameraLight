b8446df0be8ff36e3eb8e5b528ce26b236b779fe
Mantas <syenergyy@gmail.com>
2019-09-02 21:58:16 +0300

    Update KK_HCameraLight.cs

--- KK_HCameraLight.cs
+++ KK_HCameraLight.cs
@@ -3,43 +3,40 @@ using Harmony;
 using BepInEx;
 using BepInEx.Logging;
 
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
-using System.Collections;
-
 using TMPro;
 
-[BepInPlugin(nameof(KK_HCameraLight), nameof(KK_HCameraLight), "1.1")]
+[BepInPlugin(nameof(KK_HCameraLight), nameof(KK_HCameraLight), "1.2")]
 public class KK_HCameraLight : BaseUnityPlugin {
-
-    int tries;
-
-    bool inH;
-    bool lightLocked;
+    static bool inH;
+    static bool menuCreated;
+    static bool lightLocked;
 
     static Transform cameraLightTransform;
 
-    Vector3 savedPosition;
-    Quaternion savedAngle;
-    TextMeshProUGUI btnText;
+    static Vector3 savedPosition;
+    static Quaternion savedAngle;
+    static TextMeshProUGUI btnText;
 
-    Button.ButtonClickedEvent verticalReset;
-    Button.ButtonClickedEvent horizontalReset;
+    static Button.ButtonClickedEvent verticalReset;
+    static Button.ButtonClickedEvent horizontalReset;
 
     void Awake() {
         HarmonyInstance.Create(nameof(KK_HCameraLight)).PatchAll(typeof(KK_HCameraLight));
     }
 
-    void BtnClick() {
+    static void BtnClick() {
         lightLocked = !lightLocked;
 
         if (lightLocked) {
             savedPosition = cameraLightTransform.position;
             savedAngle = cameraLightTransform.rotation;
-        } else {
+        }else {
             verticalReset.Invoke();
             horizontalReset.Invoke();
         }
@@ -47,76 +44,52 @@ public class KK_HCameraLight : BaseUnityPlugin {
         btnText.text = lightLocked ? "Unlock Light" : "Lock Light";
     }
 
-    IEnumerator InitMenu(float time) {
-        yield return new WaitForSeconds(time);
+    static void CreateMenu() {
+        var light = GameObject.Find("Canvas/SubMenu/LightGroup/light").transform;
+        if (light == null) return;
+        
+        verticalReset = light.transform.Find("Vertical").GetChild(2).GetComponent<Button>().onClick; // get vertical reset for invoking
+        horizontalReset = light.transform.Find("Horizontal").GetChild(2).GetComponent<Button>().onClick; // get horizontal reset for invoking
 
-        try {
-            GameObject canvas = GameObject.Find("Canvas");
-            if (canvas != null) {
-                GameObject lightMenu = canvas.transform.GetChild(15).GetChild(1).GetChild(0).gameObject;
+        Transform copiedEntry = Instantiate(light.Find("Power"), light, false);
+        copiedEntry.name = "LockUnlock light";
+        copiedEntry.gameObject.name = "LockUnlock light";
 
-                if (lightMenu != null) {
-                    verticalReset = lightMenu.transform.GetChild(3).GetChild(2).GetComponent<Button>().onClick; // get vertical reset for invoking
-                    horizontalReset = lightMenu.transform.GetChild(4).GetChild(2).GetComponent<Button>().onClick; // get horizontal reset for invoking
+        var textShape = copiedEntry.GetChild(0);
+        textShape.SetParent(null);
+        Destroy(textShape.gameObject);
 
-                    Transform copiedEntry = Instantiate(lightMenu.transform.GetChild(5)); // copy the strength entry
-                    copiedEntry.parent = lightMenu.transform;
-                    copiedEntry.localScale = new Vector3(1f, 1f, 1f);
-                    copiedEntry.name = "LockUnlock light";
-                    copiedEntry.gameObject.name = "LockUnlock light";
+        var Slider = copiedEntry.GetChild(0);
+        Slider.SetParent(null);
+        Destroy(Slider.gameObject);
 
-                    var textShape = copiedEntry.GetChild(0); // remove title
-                    textShape.parent = null;
-                    Destroy(textShape);
+        var copiedBtn = copiedEntry.GetChild(0);
+        copiedBtn.GetComponent<RectTransform>().anchorMin = new Vector2(0.24f, 0f);
 
-                    var Slider = copiedEntry.GetChild(0); // remove slider
-                    Slider.parent = null;
-                    Destroy(Slider);
+        btnText = copiedBtn.GetChild(0).GetComponent<TextMeshProUGUI>();
+        btnText.text = lightLocked ? "Unlock Light" : "Lock Light";
 
-                    var copiedBtn = copiedEntry.GetChild(0); // fix size
-                    copiedBtn.GetComponent<RectTransform>().anchorMin = new Vector2(0.24f, 0f);
+        var uiBtn = copiedBtn.GetComponent<Button>();
 
-                    btnText = copiedBtn.GetChild(0).GetComponent<TextMeshProUGUI>(); // set text
-                    btnText.text = lightLocked ? "Unlock Light" : "Lock Light";
+        for (int i = 0; i < uiBtn.onClick.GetPersistentEventCount(); i++)
+            uiBtn.onClick.SetPersistentListenerState(i, UnityEventCallState.Off);
 
-                    var uiBtn = copiedBtn.GetComponent<Button>();
-
-                    for (int i = 0; i < uiBtn.onClick.GetPersistentEventCount(); i++)
-                        uiBtn.onClick.SetPersistentListenerState(i, UnityEventCallState.Off);
-
-                    uiBtn.onClick.AddListener(BtnClick);
-                }
-            }
-        }
-        catch(UnityException) {
-            tries--;
-
-            if(tries > 0) {
-                StartCoroutine(InitMenu(1.5f));
-            } else {
-                BepInEx.Logger.Log(LogLevel.Message, "KK_HCameraLight failed creating UI button!");
-            }
-        }
+        uiBtn.onClick.AddListener(BtnClick);
     }
 
     void KK_HFixes_LoadScene(Scene Scene, LoadSceneMode mode) {
-        if (!inH && (Scene.name == "H")) {
-            inH = true;
-            lightLocked = false;
+        if (inH || (Scene.name != "H")) return;
+        
+        inH = true;
+        menuCreated = false;
+        lightLocked = false;
 
-            savedPosition = Vector3.zero;
-            savedAngle = Quaternion.Euler(0f, 0f, 0f);
-
-            tries = 5;
-
-            StartCoroutine(InitMenu(4f));
-        }
+        savedPosition = Vector3.zero;
+        savedAngle = Quaternion.Euler(0f, 0f, 0f);
     }
 
     void KK_HFixes_UnloadScene(Scene Scene) {
-        if (inH && (Scene.name == "H")) {
-            inH = false;
-        }
+        if (inH && (Scene.name == "H")) inH = false;
     }
 
     void OnEnable() {
@@ -130,15 +103,27 @@ public class KK_HCameraLight : BaseUnityPlugin {
     }
 
     void LateUpdate() {
-        if (lightLocked && inH && cameraLightTransform != null) {
-            cameraLightTransform.position = savedPosition;
-            cameraLightTransform.rotation = savedAngle;
-        }
+        if (!lightLocked || !inH || cameraLightTransform == null) return;
+        
+        cameraLightTransform.position = savedPosition;
+        cameraLightTransform.rotation = savedAngle;
     }
 
     [HarmonyPostfix, HarmonyPatch(typeof(HSceneProc), "SetShortcutKey")]
+    [UsedImplicitly]
     public static void SetLightParent(HSceneProc __instance) {
         cameraLightTransform = __instance.lightCamera.transform;
     }
 
+    [HarmonyPrefix, HarmonyPatch(typeof(HSceneProc), "ShortCut")]
+    [UsedImplicitly]
+    public static void CreateMenuPatch(HSceneProc __instance) {
+        if (inH && !__instance.sprite.isFade && __instance.sprite.GetFadeKindProc() != HSprite.FadeKindProc.OutEnd) {
+            if (!menuCreated) {
+                menuCreated = true;
+                
+                CreateMenu();
+            }
+        }
+    }
 }
